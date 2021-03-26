f36416460c53ce0301b7d2454d96b0c9bf8d73f3
danaAlcala <bigdalcala@gmail.com>
2021-03-26 00:16:40 -0700

    Move everyone over to bchnft.art

---
+++ JLV/artwork.js
@@ -0,0 +1,164 @@
+const ARTWORK = [
+  {
+    "Name": "Kobe Bryant",
+    "Link": "https://www.juungle.net/#/assets/f9fbfd3678968e732d9e15172bfba48e6cccc34be012df7f82b18276240a0154",
+    "Image": "webKobeBryant",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Gogo",
+    "Link": "https://www.juungle.net/#/assets/7367ce208fd36ad3156f84c94a7367a34e3f877a3a09533f39876d5758b2edd5",
+    "Image": "webGogo",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Harley Quinn",
+    "Link": "https://www.juungle.net/#/assets/738e9b29d324f2974eb192edd3e45a1cdcc06631ed76eca01f0a2691e8964ffe",
+    "Image": "webHarleyQuinn",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Taarna and Elinore",
+    "Link": "https://www.juungle.net/#/assets/90a199d6cadc8ae3d3acbb98d04f2ed041aa8e1e82db6ed23324d8019475aed9",
+    "Image": "webTaarnaElinore",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Björk",
+    "Link": "https://www.juungle.net/#/assets/161dcc7567170ed9166b526259c0a6925ae058a285ad13286640a92f032f733b",
+    "Image": "webBjörk",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Santanico Pandemonium",
+    "Link": "https://www.juungle.net/#/assets/5c6870fee07b0dabc5baf5aea6a2ff0091151132c4f9b1ac13ecd7054988465a",
+    "Image": "webSantanico",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Snape",
+    "Link": "https://www.juungle.net/#/assets/6acac1edaf0003cf9d898ade2c54e1a5c1bf502842f7e7f252fb0e5fce3ac8d1",
+    "Image": "webSnape",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Aragorn and Arwen",
+    "Link": "https://www.juungle.net/#/assets/032639bb022487f221e720848bdcb66f244aa3db02575cf90511f09f4bde7ac7",
+    "Image": "webAragornArwen",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Baby Yoda",
+    "Link": "https://www.juungle.net/#/assets/8c9678501bf8cad3f830f203e54c244eca986bc05a7162522c1c1fffaaf32943",
+    "Image": "webBabyYoda",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Bane",
+    "Link": "https://www.juungle.net/#/assets/e5a0dd53e494a39429f935c2829b77077d02302b90391e47554a38c4b0118fa9",
+    "Image": "webBane",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Batman West",
+    "Link": "https://www.juungle.net/#/assets/a5d1661cee66f6452fb1a5d327b065c6538688fce8b82b7f2434e573651aa8b9",
+    "Image": "webBatman_West",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Beast",
+    "Link": "https://www.juungle.net/#/assets/559b8c2c703bdc9fe0e872e2400adc26270487b4b91debf90184ed51c66f144f",
+    "Image": "webBeast",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Believer",
+    "Link": "https://www.juungle.net/#/assets/adbbb009424b968ea150ecb6c2d79ad4d9cd47f2b6c253cdf1566295790550c5",
+    "Image": "webBeliever",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Big Ern",
+    "Link": "https://www.juungle.net/#/assets/7d698231de34bb011529aecfbcb1b990dad65516f9fce5a0123742fbd1e3bddd",
+    "Image": "webBig_Ern",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Big Worm",
+    "Link": "https://www.juungle.net/#/assets/17be76b8ba023af54538c26e95d6d0816859157d663d33fc047a8ad1b80e751a",
+    "Image": "webBig_Worm",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Black Panther",
+    "Link": "https://www.juungle.net/#/assets/5b610dcdbe8284102f548a6c017a6586b4286397e872fe4822810354b640f690",
+    "Image": "webBlackPanther",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Brodie & Svenning",
+    "Link": "https://www.juungle.net/#/assets/f3dfd05683b289b21e2d91a142de9e470ce87a5126dc461143f109fa0cf08cce",
+    "Image": "webBrodieandSvenning",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Bruce Leroy",
+    "Link": "https://www.juungle.net/#/assets/8ecc4dc5291963a0a20a9acbbe7dd9b34211eb9f890e47dabf81112bb345fb7f",
+    "Image": "webBruce_Leroy",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Buffalo Bill",
+    "Link": "https://www.juungle.net/#/assets/8f29abda8bc2ab376f168da5be8b218c8957a84ea77389c63e62f2e3c6b1ad7b",
+    "Image": "webBuffalo_Bill",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Camacho",
+    "Link": "https://www.juungle.net/#/assets/a9cf4ec321288db3a2943c68c829149c49a5a6a8a21aa4c9edf3a10d8b22c2f4",
+    "Image": "webCamacho32",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Captain Koons",
+    "Link": "https://www.juungle.net/#/assets/703cd2578f79d5b19d220b2ceee4901ce7881e893147db04029776fae54004c4",
+    "Image": "webCaptain_Koons",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "A Clockwork Orange",
+    "Link": "https://www.juungle.net/#/assets/7877281662eb7af4010ccbb6eefc12a02f9c6ad54972472b567eb96a3dbc8df8",
+    "Image": "webaclockworkorange",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Carrie Fisher - Leia",
+    "Link": "https://www.juungle.net/#/assets/8aa066ab8dce7bcdb4e07f0d3b1f66a6bd940fe66e11dad2739122e61a592332",
+    "Image": "webcarriefisherleia",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Cobra Kai",
+    "Link": "https://www.juungle.net/#/assets/e1096050c6a26ee4eba1abb31e2b3d52b87465ca5bc2018a32ed434863cd879e",
+    "Image": "webcobrakai",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "nWo",
+    "Link": "https://www.juungle.net/#/assets/88817fd7356d56421fc99d174d062fdae174af3c89a120e9095ff3833c749a8c",
+    "Image": "webnwo",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "Sho'nuff",
+    "Link": "https://www.juungle.net/#/assets/c9e3ff78ce17c9693ee176832922bafc63926b1e93715fba8ac7dd7dbe43fc71",
+    "Image": "webshonuff",
+    "Action": "Purchase with Bitcoin Cash"
+  },
+  {
+    "Name": "The Comedian",
+    "Link": "https://www.juungle.net/#/assets/963cfeb2db732c65867a181d4ac3ec1735669eaa8460a5dc8374936ff8803690",
+    "Image": "webthecomedian",
+    "Action": "Purchase with Bitcoin Cash"
+  }
+]
